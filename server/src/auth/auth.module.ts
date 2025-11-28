dacc02ad3d8a6c07291667cd5940cb47f1072033
JikwnagHan <bigaeinohu@gmail.com>
2025-11-28 16:22:11 +0900

    fix: register LoginHistory repo in auth module

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,15 +1,18 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from '../users/users.module';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './jwt.strategy';
+import { LoginHistory } from '../entities/login-history.entity';
 
 @Module({
   imports: [
     UsersModule,
     PassportModule,
+    TypeOrmModule.forFeature([LoginHistory]),
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'dev_secret_key_change_later',
       signOptions: { expiresIn: '1h' },
